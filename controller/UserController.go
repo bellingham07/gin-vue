0cd2c7f3faff94002327e20c72fc5c83876eb4c0
cao <75364055@qq.com>
2023-03-31 22:01:10 +0800

    auth,jwt,token

--- controller/UserController.go
+++ controller/UserController.go
@@ -6,9 +6,61 @@ import (
 	"gin-vue/util"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
+
+func Info(ctx *gin.Context) {
+	//获取用户信息时，用户应该是通过认证的，应该能直接从上下文中获取用户信息
+	user, _ := ctx.Get("user")
+
+	ctx.JSON(http.StatusOK, gin.H{"code": 200, "data": gin.H{"user": user}})
+}
+
+func Login(ctx *gin.Context) {
+	DB := common.GetDB()
+	//获取参数
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+	//数据验证
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		return
+	}
+
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		return
+	}
+	//判断手机号是否存在
+	var user model.User
+	DB.Where("telephone=?", telephone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
+		return
+	}
+
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误"})
+		return
+	}
+	//发放token
+	token, err := common.ReleaseToken(user)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "系统异常"})
+		log.Printf("token generate err : %v", err)
+		return
+	}
+	//返回结果
+	ctx.JSON(200, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登录成功",
+	})
+
+}
 
 func Register(ctx *gin.Context) {
 	DB := common.GetDB()
@@ -38,15 +90,21 @@ func Register(ctx *gin.Context) {
 	}
 
 	//不存在 新建用户
+	//加密密码
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误"})
+		return
+	}
 	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasedPassword),
 	}
 
 	DB.Create(&newUser)
 
-	ctx.JSON(200, gin.H{"msg": "注册成功"})
+	ctx.JSON(200, gin.H{"code": 200, "msg": "注册成功"})
 }
 
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
--- route/routes.go
+++ route/routes.go
@@ -2,10 +2,13 @@ package route
 
 import (
 	"gin-vue/controller"
+	"gin-vue/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func CollectRoute(r *gin.Engine) *gin.Engine {
-	r.POST("/api/auth/register", controller.Register)
+	r.POST("/api/auth/register", controller.Register)                     //注册
+	r.POST("/api/auth/login", controller.Login)                           //登录
+	r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info) //用中间件保护我们用户信息结构 用户信息
 	return r
 }
